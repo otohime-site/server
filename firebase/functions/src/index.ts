a2aa9d62501cee539258ee2d808a329b7eedf75d
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-10-19 17:49:20 +0800

    feat: add firebase JWT creator

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,20 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// https://github.com/hasura/graphql-engine/blob/master/community/sample-apps/firebase-jwt/functions/index.js
+export const processSignup = functions.auth.user().onCreate(async user => {
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'user',
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-user-id': user.uid
+    }
+  }
+  try {
+    await admin.auth().setCustomUserClaims(user.uid, customClaims)
+    const metadataRef = admin.database().ref(`metadata/${user.uid}`)
+    return await metadataRef.set({ refreshTime: new Date().getTime() })
+  } catch (error) {
+    console.log(error)
+  }
+})
