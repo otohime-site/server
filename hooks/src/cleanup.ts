87c944d46f5ab50ce7939a644d630a16935e6947
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-11-29 17:12:16 +0800

    feat: add player cleanup

---
+++ hooks/src/cleanup.ts
@@ -0,0 +1,30 @@
+import pool from './db'
+import Router from 'koa-router'
+const router = new Router()
+
+router.get('/', async (ctx, next) => {
+  const client = await pool.connect()
+  await client.query('BEGIN')
+  try {
+    await client.query(`
+      SELECT periods.drop_system_versioning('dx_intl_records');
+      SELECT periods.drop_system_versioning('dx_intl_scores');
+      GRANT DELETE ON TABLE dx_intl_records_history TO CURRENT_USER;
+      GRANT DELETE ON TABLE dx_intl_scores_history TO CURRENT_USER;
+      DELETE FROM dx_intl_records_history WHERE player_id NOT IN (SELECT id FROM dx_intl_players);
+      DELETE FROM dx_intl_scores_history WHERE player_id  NOT IN (SELECT id FROM dx_intl_players);
+      DELETE FROM dx_intl_scores_history WHERE note_id  NOT IN (SELECT id FROM dx_intl_notes);
+      SELECT periods.add_system_versioning('dx_intl_records');
+      SELECT periods.add_system_versioning('dx_intl_scores');
+      COMMIT;
+    `)
+  } catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  } finally {
+    client.release()
+  }
+  ctx.body = 'ok!'
+})
+
+export default router
--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -3,11 +3,13 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import authRouter from './auth'
 import fetchRouter from './fetch'
+import cleanupRouter from './cleanup'
 
 const app = new Koa()
 const router = new Router()
 router.use('/auth', authRouter.routes())
 router.use('/fetch', fetchRouter.routes())
+router.use('/cleanup', cleanupRouter.routes())
 
 app.use(router.routes()).use(router.allowedMethods())
 app.listen(8787)
