5d4f0466348c8b54a9d46ccb4d5744d3807fdc95
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-12-07 20:33:46 +0800

    feat: add index on card name and view on base rating

---
+++ migrations/1607325653549_dx-intl-stats/down.sql
@@ -0,0 +1,4 @@
+DROP VIEW dx_intl_base_rating;
+DROP INDEX card_name_idx;
+DROP VIEW dx_intl_public_scores;
+DROP VIEW dx_intl_public_records;
---
+++ migrations/1607325653549_dx-intl-stats/up.sql
@@ -0,0 +1,64 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX card_name_idx ON dx_intl_records USING GIN (card_name gin_trgm_ops);
+
+CREATE VIEW dx_intl_public_records AS
+    SELECT r.* FROM dx_intl_records r
+    INNER JOIN dx_intl_players p ON p.id = r.player_id
+    WHERE p.private = false;
+
+CREATE VIEW dx_intl_public_scores AS
+    SELECT s.* FROM dx_intl_scores s
+    INNER JOIN dx_intl_players p ON p.id = s.player_id
+    WHERE p.private = false;
+
+CREATE VIEW dx_intl_base_rating AS
+    WITH range_table AS(
+        SELECT
+        rating_range,
+        format('%s - %s', lower(rating_range), upper(rating_range) - 1) AS description
+        FROM unnest(array[
+            int4range(0, 1000),
+            int4range(1000, 2000),
+            int4range(2000, 3000),
+            int4range(3000, 4000),
+            int4range(4000, 5000),
+            int4range(5000, 5500),
+            int4range(5500, 6000),
+            int4range(6000, 6500),
+            int4range(6500, 7000),
+            int4range(7000, 7500),
+            int4range(7500, 8000),
+            int4range(8000, NULL)
+        ]) AS rating_range
+    )
+    SELECT MIN(rt.description) AS range, COUNT(r.base_rating) AS count FROM (
+        SELECT (rating - CASE
+        WHEN grade = 2 THEN 250
+        WHEN grade = 3 THEN 500
+        WHEN grade = 4 THEN 750
+        WHEN grade = 5 THEN 1000 
+        WHEN grade = 6 THEN 1200
+        WHEN grade = 7 THEN 1400
+        WHEN grade = 8 THEN 1500
+        WHEN grade = 9 THEN 1600
+        WHEN grade = 10 THEN 1700
+        WHEN grade = 11 THEN 1800
+        WHEN grade = 12 THEN 1850
+        WHEN grade = 13 THEN 1900
+        WHEN grade = 14 THEN 1950
+        WHEN grade = 15 THEN 2000
+        WHEN grade = 16 THEN 2010
+        WHEN grade = 17 THEN 2020
+        WHEN grade = 18 THEN 2030
+        WHEN grade = 19 THEN 2040 
+        WHEN grade = 20 THEN 2050
+        WHEN grade = 21 THEN 2060
+        WHEN grade = 22 THEN 2070
+        WHEN grade = 23 THEN 2080
+        WHEN grade = 24 THEN 2090
+        WHEN grade = 25 THEN 2100
+        ELSE 0
+        END) AS base_rating FROM dx_intl_public_records
+    ) r
+    INNER JOIN range_table rt ON rt.rating_range @> r.base_rating
+    GROUP BY rt.rating_range ORDER BY rt.rating_range DESC;
