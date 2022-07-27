57ccb38f0699339844becd16521c785c1210ac27
Hsiao-Ting Yu <sst.dreams@gmail.com>
2022-07-28 03:24:24 +0800

    feat: deal with song name changes

--- migrations/default/1658946364741_version_upgrade/down.sql
+++ migrations/default/1658946364741_version_upgrade/down.sql
@@ -1,2 +1,9 @@
 alter table "public"."dx_intl_variants" drop constraint "dx_intl_variants_version_check1";
 alter table "public"."dx_intl_variants" add constraint "dx_intl_variants_version_check1" CHECK (version >= 0 AND version <= 17);
+
+UPDATE dx_intl_songs SET id = '37b114067456c07e82cafbb6dee4991bd74bc0d590179db68c31c4f90fb28dce',
+title = 'GIGANTOMAKHIA' WHERE id = '19c1a9ab5da18fde0228e06eb33a889adf1cb6e71775593685975bf83f4b1a5c';
+
+alter table "public"."dx_intl_scores" drop constraint "dx_intl_scores_song_id_deluxe_difficulty_fkey";
+alter table "public"."dx_intl_scores" add constraint "dx_intl_scores_song_id_deluxe_difficulty_fkey"
+FOREIGN KEY (song_id, deluxe, difficulty) REFERENCES dx_intl_notes(song_id, deluxe, difficulty);
--- migrations/default/1658946364741_version_upgrade/up.sql
+++ migrations/default/1658946364741_version_upgrade/up.sql
@@ -1,2 +1,10 @@
 alter table "public"."dx_intl_variants" drop constraint "dx_intl_variants_version_check1";
 alter table "public"."dx_intl_variants" add constraint "dx_intl_variants_version_check1" CHECK (version >= 0 AND version <= 18);
+
+alter table "public"."dx_intl_scores" drop constraint "dx_intl_scores_song_id_deluxe_difficulty_fkey";
+alter table "public"."dx_intl_scores" add constraint "dx_intl_scores_song_id_deluxe_difficulty_fkey"
+FOREIGN KEY (song_id, deluxe, difficulty) REFERENCES dx_intl_notes(song_id, deluxe, difficulty)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+UPDATE dx_intl_songs SET id = '19c1a9ab5da18fde0228e06eb33a889adf1cb6e71775593685975bf83f4b1a5c',
+title = 'GIGANTØMAKHIA' WHERE id = '37b114067456c07e82cafbb6dee4991bd74bc0d590179db68c31c4f90fb28dce';
