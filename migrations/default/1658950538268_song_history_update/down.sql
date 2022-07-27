0a0e694720f37d026dfdda42b662bed70a3cffd0
Hsiao-Ting Yu <sst.dreams@gmail.com>
2022-07-28 03:42:37 +0800

    feat: deal with song id in history

---
+++ migrations/default/1658950538268_song_history_update/down.sql
@@ -0,0 +1,5 @@
+SELECT periods.drop_system_versioning('dx_intl_scores');
+GRANT UPDATE ON TABLE dx_intl_scores_history TO CURRENT_USER;
+UPDATE dx_intl_scores_history SET song_id = '37b114067456c07e82cafbb6dee4991bd74bc0d590179db68c31c4f90fb28dce'
+WHERE song_id = '19c1a9ab5da18fde0228e06eb33a889adf1cb6e71775593685975bf83f4b1a5c';
+SELECT periods.add_system_versioning('dx_intl_scores');
