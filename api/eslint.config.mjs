f20d83307c8df119a7a4129c2b451e7f53dbafe4
Hsiao-Ting Yu <sst.dreams@gmail.com>
2026-01-12 00:57:24 +0800

    feat: prepare new API migration

---
+++ api/eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+import eslint from "@eslint/js"
+import prettier from "eslint-config-prettier/flat"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+)
