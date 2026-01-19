dfbf5ad234087d420ed47294101c49d8523da8f6
Hsiao-Ting Yu <sst.dreams@gmail.com>
2026-01-20 00:35:56 +0800

    feat: remove new api wip for now

--- api/src/index.ts
+++
@@ -1,6 +0,0 @@
-import node from '@elysiajs/node'
-import {Elysia} from 'elysia'
-
-new Elysia({adapter: node()}).listen(8765, ({hostname, port}) => {
-    console.log(`Server running at http://${hostname}:${port}`)
-})
