0a2b32ba6398b9c5b40e62f1032ec40c5b50e69c
Hsiao-Ting Yu <sst.dreams@gmail.com>
2026-04-17 00:50:12 +0800

    Revert "fix: fix tsc error caused by https://github.com/porsager/postgres/issues/1143"
    This reverts commit a14a85c39dd1359e180ed259ac0b63ff59edd5e3.

--- hooks/src/refresh.ts
+++ hooks/src/refresh.ts
@@ -5,9 +5,7 @@ const app = new Hono()
 
 app.post("/", async (c) => {
   await sql.begin(async (tx) => {
-    // @ts-expect-error https://github.com/porsager/postgres/issues/1143
     await tx`REFRESH MATERIALIZED VIEW CONCURRENTLY dx_intl_scores_stats;`
-    // @ts-expect-error https://github.com/porsager/postgres/issues/1143
     await tx`REFRESH MATERIALIZED VIEW CONCURRENTLY dx_intl_scores_rates;`
   })
   return c.text("ok!")
