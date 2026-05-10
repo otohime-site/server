245079d0f44e09dd6766a393508dcdd29ed6adcf
Hsiao-Ting Yu <sst.dreams@gmail.com>
2026-05-10 16:29:17 +0800

    feat: add `dx_intl_rating_target_stats` into refresh

--- hooks/src/refresh.ts
+++ hooks/src/refresh.ts
@@ -7,6 +7,7 @@ app.post("/", async (c) => {
   await sql.begin(async (tx) => {
     await tx`REFRESH MATERIALIZED VIEW CONCURRENTLY dx_intl_scores_stats;`
     await tx`REFRESH MATERIALIZED VIEW CONCURRENTLY dx_intl_scores_rates;`
+    await tx`REFRESH MATERIALIZED VIEW CONCURRENTLY dx_intl_rating_target_stats;`
   })
   return c.text("ok!")
 })
